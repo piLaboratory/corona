358fea6c9007ed5a9f06ec4c854625806315a991
piklprado <paulo@PI.localdomain>
2020-03-14 21:09:57 -0300

    Calculos para China

--- china_italia_brasil.R
+++ china_italia_brasil.R
@@ -60,3 +60,44 @@ log(2)/coef(brasil.fim.fit)[2]
 ## Intervalos de confianca
 ## Fim
 log(2)/confint(brasil.fim.fit)[2,]
+
+################################################################################
+## China
+################################################################################
+china.raw <- read.csv("https://covid.ourworldindata.org/data/full_data.csv", as.is=TRUE) %>%
+    filter(location=="China") %>%
+    select(date,total_cases)
+
+china <- zoo(x = china.raw$total_cases,
+             order.by = as.Date(china.raw$date, "%Y-%m-%d"))
+## Um plot da serie temporal completa
+plot(china, type="p")
+
+## Minimo de casos para o dia zero
+china.n.casos <- 15
+## N de dias para incluir no ajuste
+dias.final <- 7
+## Ajuste a partir do dia zero
+china.dia.zero <- min(which(china>=china.n.casos, arr.ind=TRUE))
+## Ajuste do modelo aos primeiros 7 dias a partir do dia zero
+x <- 0:(dias.final-1)
+y <- china[china.dia.zero:(china.dia.zero+dias.final-1)]
+## Ajuste do modelo Poisson
+## Inicio do periodo
+china.inicio.fit <- glm(y~x, family=poisson)
+## Ultimos 7 dias
+y <- china[(length(china)-dias.final+1):length(china)]
+china.fim.fit <- glm(y~x, family=poisson)
+## Coeficientes
+coef(china.inicio.fit)[2]
+coef(china.fim.fit)[2]
+## tempos de duplicacao ##
+## Inicio
+log(2)/coef(china.inicio.fit)[2]
+## Atual
+log(2)/coef(china.fim.fit)[2]
+## Intervalos de confianca
+## Inicio
+log(2)/confint(china.inicio.fit)[2,]
+## Fim
+log(2)/confint(china.fim.fit)[2,]
